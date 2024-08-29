c749e4890755cb6fa2cb0aaf452f734a4009c6ac
Winner Edwin <michealanthonyxxx@gmail.com>
2024-08-29 06:33:30 +0100

    Included build script for Windows

--- src/main.tsx
+++ src/main.tsx
@@ -8,48 +8,51 @@ import { Provider } from "react-redux"
 import Store, { addDevice, setMain, setRouteError } from './context'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const client = new QueryClient({
-  // defaultOptions: {
-  //   queries: {
-  //     retry: process.env.NODE_ENV === "test" ? false : 3
-  //   }
-  // }
-})
 
-
-// load startup data
-try {
-  const res = await fetch("/api/devices")
-  if (!res.ok) {
-    Store.dispatch(setRouteError({
-      message: await res.text(),
-      status: res.status
-    }))
-  } else {
-    const devices: Device[] = await res.json();
-    if (!devices.length) throw null
-    Store.dispatch(addDevice(devices))
-    // set or confirm main device
-    const match = /^.+\/devices\/(.+)\/.*/
-    if (!match.test(location.href))
-      Store.dispatch(setMain(devices[0]))
-    else {
-      const id = location.href.match(match)![1]
-      const device = devices.find((device) => device.id === id);
-      if (device) Store.dispatch(setMain(device))
+(async function () {
+  // load startup data
+  try {
+    const res = await fetch("/api/devices")
+    if (!res.ok) {
+      Store.dispatch(setRouteError({
+        message: await res.text(),
+        status: res.status
+      }))
+    } else {
+      const devices: Device[] = await res.json();
+      if (!devices.length) throw null
+      Store.dispatch(addDevice(devices))
+      // set or confirm main device
+      const match = /^.+\/devices\/(.+)\/.*/
+      if (!match.test(location.href))
+        Store.dispatch(setMain(devices[0]))
+      else {
+        const id = location.href.match(match)![1]
+        const device = devices.find((device) => device.id === id);
+        if (device) Store.dispatch(setMain(device))
+      }
     }
-  }
-  // eslint-disable-next-line no-empty
-} catch { }
+    // eslint-disable-next-line no-empty
+  } catch { }
+})()
+  .then(() => {
+    const client = new QueryClient({
+      // defaultOptions: {
+      //   queries: {
+      //     retry: process.env.NODE_ENV === "test" ? false : 3
+      //   }
+      // }
+    })
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <HelmetProvider>
-      <Provider store={Store}>
-        <QueryClientProvider client={client}>
-          <RouterProvider router={router} />
-        </QueryClientProvider>
-      </Provider>
-    </HelmetProvider>
-  </StrictMode>,
-)
+    createRoot(document.getElementById('root')!).render(
+      <StrictMode>
+        <HelmetProvider>
+          <Provider store={Store}>
+            <QueryClientProvider client={client}>
+              <RouterProvider router={router} />
+            </QueryClientProvider>
+          </Provider>
+        </HelmetProvider>
+      </StrictMode>,
+    )
+  })
